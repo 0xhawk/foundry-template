eaf863e300fa887119f6e40baeda2e12bbb526a4
0xhawk <hawk@horizonx.tech>
2022-12-14 10:36:58 +0800

    add interfaces

--- src/proxies/IProxy.sol
+++ src/proxies/IProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 /// @title IProxy - Helper interface to access masterCopy of the Proxy on-chain
 interface IProxy {
