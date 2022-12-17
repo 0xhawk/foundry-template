1cc1379ec349c43659c6f54f876baf40312e7da2
0xhawk <hawk@horizonx.tech>
2022-12-17 13:15:17 +0400

    add counter to mint tokenId

--- src/VoiceFacade.sol
+++ src/VoiceFacade.sol
@@ -2,17 +2,22 @@
 pragma solidity 0.8.17;
 
 import "./base/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract VoiceFacade is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     function initialize(string memory name, string memory baseURI) external {
         _name = name;
         _symbol = string(abi.encodePacked("VoiStock_", name));
         _baseURI = baseURI;
-        // TODO: mint
         // TODO: initialize
     }
 
-    function newVoice() external {
-        // TODO: mint
+    function newVoice(address minter) external {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(minter, newTokenId);
     }
 }
